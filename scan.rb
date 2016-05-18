be97c16a1ee1ce2d686aa514aeebb54cdc38d795
dkhamsing <dkhamsing8@gmail.com>
2016-05-18 15:29:09 +0000

    update [ci skip]

--- scan.rb
+++ scan.rb
@@ -1,24 +1,34 @@
 require 'awesome_bot'
 
-r, f = ARGV
+if ARGV.count==0
+    puts 'usage: scan <input> <output>'
+    exit 
+end
 
-warn r
-warn f
+r, f = ARGV
+f = 'temp' if f.nil?
+
+warn 'input: ' + r
+warn 'output: ' + f
 
 c = File.read r
 links = AwesomeBot.links_find c
 warn links
-exit if links.count == 0
-link = links[0]
 
-warn link
+if links.count == 0
+    puts 'no links found'
+    exit 
+end
+
+link = links[0]
+warn 'link: ' + link
 
 require 'net/http'
 require 'uri'
 
 b = Net::HTTP.get(URI.parse(link))
-
 warn b
 
+puts 'Writing ' + f
 File.write f, b
 puts '👍'
