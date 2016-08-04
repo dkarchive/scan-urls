eb271e5c86cc669885d5c6cc43a72c1c69ad0306
Daniel Khamsing <dkhamsing@cirruspath.com>
2016-08-04 06:39:11 -0700

    :mag: [ci skip]

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # scan-urls
-Scan URLs 
+
+"On-demand" [`awesome_bot`](https://github.com/dkhamsing/awesome_bot) scans :mag:
+
+## Usage 
+
+To scan URLs on a page, create a pull request to edit this README and paste its URL. Results will be posted on the [pull request](https://github.com/dkhamsing/scan-urls/pull/5).
+
+## Contact
+
+- [github.com/dkhamsing](https://github.com/dkhamsing)
+- [twitter.com/dkhamsing](https://twitter.com/dkhamsing)
+
+<!-- 
+
+PASTE URL HERE:
+
+-->
---
+++ scan.rb
@@ -0,0 +1,46 @@
+require 'awesome_bot'
+
+if ARGV.count==0
+    puts 'usage: scan <input> <output>'
+    exit 
+end
+
+SKIP = [
+    'https://github.com/dkhamsing/awesome_bot', 
+    'https://github.com/dkhamsing/scan-urls/pull/3',
+    'https://github.com/dkhamsing',
+    'https://twitter.com/dkhamsing'
+]
+
+r, f = ARGV
+f = 'temp' if f.nil?
+
+warn 'input: ' + r
+warn 'output: ' + f
+
+c = File.read r
+
+SKIP.each do |x|
+  c = c.sub x, ''
+end
+
+links = AwesomeBot.links_find c
+warn 'links:' + links.to_s
+
+if links.count == 0
+    puts 'no links found'
+    exit 
+end
+
+link = links[0]
+warn 'link: ' + link
+
+require 'net/http'
+require 'uri'
+
+b = Net::HTTP.get(URI.parse(link))
+warn b
+
+puts 'Writing ' + f
+File.write f, b
+puts '👍 '
